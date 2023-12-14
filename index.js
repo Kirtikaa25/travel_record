d7f90cba424c4b63bcda54258d03f2b20614d298
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-15 02:12:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
+  password: "KI25@ika",
   port: 5432,
 });
 db.connect();
@@ -20,7 +20,7 @@ app.use(express.static("public"));
 let currentUserId = 1;
 
 let users = [
-  { id: 1, name: "Kirtika", color: "powderblue" },
+  { id: 1, name: "Kirtika", color: "teal" },
   { id: 2, name: "Rishi", color: "teal" },
 ];
 
@@ -32,17 +32,27 @@ async function checkVisisted() {
   });
   return countries;
 }
+async function getCurrentUser() {
+  const result = await db.query("SELECT * FROM users");
+  users = result.rows;
+  return users.find((user) => user.id == currentUserId);
+}
+
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
+  const currentUser = await getCurrentUser();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
     users: users,
-    color: "teal",
+    color: currentUser.color,
   });
 });
+
+
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
+  const currentUser = await getCurrentUser();
 
   try {
     const result = await db.query(
@@ -54,8 +64,8 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
       );
       res.redirect("/");
     } catch (err) {
@@ -63,6 +73,14 @@ app.post("/add", async (req, res) => {
     }
   } catch (err) {
     console.log(err);
+  }
+});
+app.post("/user", async (req, res) => {
+  if (req.body.add === "new") {
+    res.render("new.ejs");
+  } else {
+    currentUserId = req.body.user;
+    res.redirect("/");
   }
 });
 
@@ -80,6 +98,11 @@ app.post("/add", async (req, res) => {
   
     res.redirect("/");
   });
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
 
 
 app.listen(port, () => {
