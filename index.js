8b8843f406be1346cf409de0257b49ab2a63c1b7
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-15 02:02:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,20 @@ app.post("/add", async (req, res) => {
 app.post("/user", async (req, res) => {});
 
 app.post("/new", async (req, res) => {
- 
+   app.post("/new", async (req, res) => {
+    const name = req.body.name;
+    const color = req.body.color;
+  
+    const result = await db.query(
+      "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
+      [name, color]
+    );
+  
+    const id = result.rows[0].id;
+    currentUserId = id;
+  
+    res.redirect("/");
+  });
 });
 
 app.listen(port, () => {
