0bba8411ad2e8cd1f96235a1142526dceca443be
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2024-02-05 14:45:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world"
+  database: "world",
   password: "password", // your postgres login password
   port: 5432,
 });
